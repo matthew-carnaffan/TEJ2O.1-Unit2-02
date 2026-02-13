4130c1650b83f4cb4efa516a2f8d19af21fad7b2
matthew-carnaffan <matthew.carnaffan@stu.ocsb.ca>
2026-02-13 12:43:19 -0500

    I changed "Hello, world" to "Hello, MTHS"

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Matthew Carnaffan
+ * Created on: Feb 2026
+ * This program says "Hello, MTHS!"
 */
 
-basic.showString('Hello, World!')
+basic.showString('Hello, MTHS!')
